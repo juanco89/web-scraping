c835d0b72aae00743d059fa0bb2c92c9b5ed0d88
Juan C. Orozco <juanco89@gmail.com>
2018-04-28 12:54:02 -0500

    Se agregan archivos del web scraping.
    Ejecutar:
    virtualenv venv
    pip install -r requirements.txt
    pyhton web_scraping.py

---
+++ web_scraping.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+
+def run():
+    target_folder = os.path.abspath('imgs/')
+    if not(os.path.exists(target_folder)):
+        print('mkdir {}'.format(target_folder))
+        os.mkdir(target_folder)
+
+    for i in range(1, 5):
+        response = requests.get('https://xkcd.com/{}'.format(i))
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_container = soup.find(id='comic')
+
+        image_url = image_container.find('img')['src']
+        image_name = image_url.split('/')[-1]
+
+        print('Downloading image {}'.format(image_name))
+
+        urllib.request.urlretrieve('https:{}'.format(image_url), target_folder + image_name)
+
+
+if __name__ == '__main__':
+    run()
